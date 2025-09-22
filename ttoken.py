ac96d5e69efa7e2e79da6d38959c5737cf60cef5
Augusto Leal <augustoleal11@gmail.com>
2025-09-22 12:50:58 -0300

    ttoken prontos(eu acho)

--- ttoken.py
+++ ttoken.py
@@ -1,44 +1,46 @@
-#---------------------------------------------------
-# Tradutor para a linguagem CALC
-#
-# versao 1a (mar-2024)
-#---------------------------------------------------
-
 from enum import IntEnum
 class TOKEN(IntEnum):
     erro = 1
     eof = 2
     ident = 3
-    num = 4
-    string = 5
-    IF = 6
-    ELSE = 7
-    INICIO = 8
-    FIM = 9
-    PROG = 10
-    abrePar = 11
-    fechaPar = 12
-    virg = 13
-    ptoVirg = 14
-    pto = 15
-    atrib = 16
-    igual = 17
-    diferente = 18
-    menor = 19
-    menorIgual = 20
-    maior = 21
-    maiorIgual = 22
-    AND = 23
-    OR = 24
-    NOT = 25
-    mais = 26
-    menos = 27
-    multiplica = 28
-    divide = 29
-    LEIA = 30
-    ESCREVA = 31
-    abreChave = 32
-    fechaChave = 33
+    INT = 4
+    FLOAT = 5
+    CHAR = 6
+    valorInt = 7
+    valorFloat = 8
+    valorChar = 9
+    valorString = 10
+    IF = 11
+    ELSE = 12
+    WHILE = 13
+    RETURN = 14
+    FOR = 15
+    BREAK = 16
+    CONTINUE = 17
+    abrePar = 18
+    fechaPar = 19
+    virg = 20
+    ptoVirg = 21
+    pto = 22
+    atrib = 23
+    igual = 24
+    diferente = 25
+    menor = 26
+    menorIgual = 27
+    maior = 28
+    maiorIgual = 29
+    AND = 30
+    OR = 31
+    NOT = 32
+    mais = 33
+    menos = 34
+    multiplica = 35
+    divide = 36
+    modulo = 37
+    abreChave = 38
+    fechaChave = 39
+    abrecolchete = 40
+    fechacolchete = 41
 
 
     @classmethod
@@ -47,52 +49,64 @@ class TOKEN(IntEnum):
             1:'erro',
             2:'<eof>',
             3:'ident',
-            4:'numero',
-            5:'string',
-            6:'if',
-            7:'else',
-            8:'inicio',
-            9:'fim',
-            10:'prog',
-            11:'(',
-            12:')',
-            13:',',
-            14:';',
-            15:'.',
-            16:'=',
-            17:'==',
-            18:'!=',
-            19:'<',
-            20:'<=',
-            21:'>',
-            22:'>=',
-            23:'and',
-            24:'or',
-            25:'not',
-            26:'+',
-            27:'-',
-            28:'*',
-            29:'/',
-            30:'leia',
-            31:'escreva',
-            32:'{',
-            33:'}',
+            4:'int',
+            5:'float',
+            6:'char',
+            7:'valorInt',
+            8:'valorFloat',
+            9:'valorChar',
+            10:'valorString',
+            11:'if',
+            12:'else',
+            13:'while',
+            14:'program',
+            15:'for',
+            16:'break',
+            17:'continue',
+            18:'(',
+            19:')',
+            20:',',
+            21:';',
+            22:'.',
+            23:'=',
+            24:'==',
+            25:'!=',
+            26:'<',
+            27:'<=',
+            28:'>',
+            29:'>=',
+            30:'&&', # and
+            31:'||', # or
+            32:'!', # not  
+            33:'+',
+            34:'-',
+            35:'*',
+            36:'/',
+            37:'%',
+            38:'{',
+            39:'}',
+            40:'[',
+            41:']',
+
         }
         return nomes[token]
 
     @classmethod
     def reservada(cls, lexema):
         reservadas = {
-            'prog': TOKEN.PROG,
             'if': TOKEN.IF,
-            'inicio': TOKEN.INICIO,
-            'fim': TOKEN.FIM,
             'else': TOKEN.ELSE,
-            'leia': TOKEN.LEIA,
-            'escreva': TOKEN.ESCREVA,
             'and': TOKEN.AND,
             'or': TOKEN.OR,
-            'not': TOKEN.NOT
+            'not': TOKEN.NOT,
+            'while': TOKEN.WHILE,
+            'return': TOKEN.RETURN,
+            'int': TOKEN.INT,
+            'float': TOKEN.FLOAT,
+            'char': TOKEN.CHAR,
+            'for': TOKEN.FOR,
+            'break': TOKEN.BREAK,
+            'continue': TOKEN.CONTINUE,
         }
         if lexema in reservadas:
             return reservadas[lexema]
